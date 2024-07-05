a5c1f05ab5148c6184024254e3589006f73736f9
fathima-nihala <137775396+fathima-nihala@users.noreply.github.com>
2024-07-05 15:16:07 +0530

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  axios.defaults.withCredentials = true;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('https://deploy-mern-api.vercel.app/register', { name, email, password })
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded" style={{ minWidth: '300px' }}>
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              autoComplete="off"
+              name="name"
+              className="form-control rounded-0"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              className="form-control rounded-0"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0 mb-2">
+            Register
+          </button>
+          <p className="text-center mb-1">Already Have an Account</p>
+          <button type="button" className="btn btn-default border w-100 bg-light rounded-0">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,42 @@
+//0MDB19PS7ZDGGq2A
+//mongodb+srv://fathimanihala200214:0MDB19PS7ZDGGq2A@cluster0.aqc4dnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const RegisterModel = require('./modals/Register')
+
+const app = express()
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://fathimanihala200214:0MDB19PS7ZDGGq2A@cluster0.aqc4dnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("Already have an account")
+        } else {
+            RegisterModel.create({name: name, email: email, password: password})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+        }
+    }).catch(err => res.json(err))
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
