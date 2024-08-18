8f663312ed175f9f277c874c980650a0d006ceed
Nihala <fathimanihala200214@gmail.com>
2024-08-18 22:18:42 +0530

    init

--- server/index.js
+++ server/index.js
@@ -18,7 +18,9 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
 
 
 app.get("/", (req, res) => {
