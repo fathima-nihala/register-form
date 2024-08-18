bd164feaa3dce3290a79e9715995299a27e67b57
Nihala <fathimanihala200214@gmail.com>
2024-08-18 22:15:15 +0530

    init

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const RegisterModel = require('./modals/Register')
+require('dotenv').config();
+
 
 const app = express()
 app.use(cors(
